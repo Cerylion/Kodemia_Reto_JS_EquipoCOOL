0519c2a8903bcc5208b3c9fb2fa40560c4c392c1
Cerylion <fullthrottle.moran@gmail.com>
2024-02-10 14:54:25 -0600

    sirve el buscador - arreglando estilos de posts

--- loscodigoscool.js
+++ loscodigoscool.js
@@ -4,41 +4,31 @@ document.addEventListener('DOMContentLoaded', () => {
       const respuesta = await fetch(
         `https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/champion/${campeon}.json`
       )
-
       if (!respuesta.ok) {
         throw new Error(`Error al obtener los datos del campeón ${campeon}`)
       }
-
       const datos = await respuesta.json()
       const campeonData = datos.data[campeon]
-
       if (!campeonData || !campeonData.skins) {
         throw new Error(`Datos de skins del campeón ${campeon} no encontrados`)
       }
-
       const skins = campeonData.skins
       const skinAzar = skins[Math.floor(Math.random() * skins.length)] // para seleccionar una al azar
-
       return `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${campeon}_${skinAzar.num}.jpg`
     } catch (error) {
       console.error('Error:', error)
-
       return null
     }
   }
 
   async function mostrarImagen() {
     const imagenURL = await obtenerImagenAzar('Kled')
-
     if (imagenURL) {
       const imagen = document.createElement('img')
-
       imagen.src = imagenURL
-      //imagen.className = 'profileImage'
-      
-
-      document.getElementById('insertIMG').src = imagenURL
-      //document.body.appendChild(imagen)
+      //imagen.className = 'profileImage' esto le agrega la clase a la imagen
+      document.getElementById('insertIMG').src = imagenURL // esto es lo que mete la imagen dentro del cuadrito - no se puede hacer con la clase
+      //document.body.appendChild(imagen) esto mete la imagen al final del doc
     } else {
       console.log('No se pudo obtener la imagen.')
     }
@@ -46,18 +36,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   mostrarImagen()
 
+  // la magia de los post
+
   const form = document.querySelector('form')
   const tituloImput = document.getElementById('titulo')
   const articuloImput = document.getElementById('articulo')
   const bodyTabla = document.getElementById('bodyTabla')
 
   let data = JSON.parse(localStorage.getItem('formData')) || []
-  //El método JSON.parse() analiza una cadena de texto como JSON, transformando opcionalmente el valor producido por el análisis.
-  //El método getItem() de la interfaz Storage devuelve el valor de la clave cuyo nombre se le pasa por parámetro.
 
   form.addEventListener('submit', function (e) {
     e.preventDefault()
-    //Cancela el evento si este es cancelable, sin detener el resto del funcionamiento del evento, es decir, puede ser llamado de nuevo.
     const titulo = tituloImput.value
     const articulo = articuloImput.value
 
@@ -70,39 +59,117 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
-  //aquí comienza la locura
   function saveDataLocalStorage() {
     localStorage.setItem('formData', JSON.stringify(data))
   }
-  //El método JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON,
-  //opcionalmente reemplaza valores si se indica una función de reemplazo, o si se especifican las propiedades mediante un array de reemplazo.
-  //El método setItem() de la interfaz Storage, cuando reciba una clave y un valor, añadirá estos al almacén, o actualizará el valor si la clave ya existe.
 
-  const  generarTabla = () => {
+  function generarTabla() {
     bodyTabla.innerHTML = ''
-    const objeto = document.getElementById('0')
-    
-    localStorage.setItem('objeto', objeto)
-    const newObject = localStorage.posts ? JSON.parse(localStorage.posts) : []
-    /* const newObject = JSON.parse(localStorage.objeto) */
-    console.log(newObject)
-
 
     data.forEach(function (item, index) {
-      const fila = document.createElement('tr')
-      const celdaTitulo = document.createElement('td')
-      const celdaArticulo = document.createElement('td')
-      const accionesBoton = document.createElement('td')
+      //gran contenedor//
+
+      const divGranContenedor = document.createElement('div')
+      //imagen
+      divGranContenedor.className = 'juniorDevSectionPadding'
+      const divImagen = document.createElement('div')
+      divImagen.className = 'imagenDeUsuario'
+      const creaImagen = document.createElement('img')
+      creaImagen.className = 'imagenUsuario'
+      creaImagen.type = 'img'
+      bodyTabla.appendChild(divImagen)
+      divImagen.appendChild(creaImagen)
+
+      //nombre
+      const divNombre = document.createElement('div')
+      divNombre.className = 'nombreUsuario'
+      const divNombreH3 = document.createElement('H3')
+      divNombreH3.textContent = '🦄 Nombre del usuario'
+      divNombre.appendChild(divNombreH3)
+      bodyTabla.appendChild(divNombre)
+
+      //contenedorPost//
+      const fila = document.createElement('div')
+      fila.className = 'contenedorPost'
+      fila.classname = 'juniorDevSectionPadding'
+
+      //titulo
+      const celdaTitulo = document.createElement('span')
+      celdaTitulo.className = 'tituloPost'
+
+      //articulo
+      const celdaArticulo = document.createElement('span')
+      celdaArticulo.className = 'articuloPost'
+
+      //tags
+      const tagsSection = document.createElement('div')
+      tagsSection.className = 'tagsAside'
+      const tags1 = document.createElement('p')
+      tags1.className = 'tag1'
+      tags1.textContent = '#JavaScrip'
+      const tags2 = document.createElement('p')
+      tags2.className = 'tag2'
+      tags2.textContent = '#Develop'
+      const tags3 = document.createElement('p')
+      tags3.className = 'tag3'
+      tags3.textContent = '#Soprendente!'
+      tagsSection.appendChild(tags1)
+      tagsSection.appendChild(tags2)
+      tagsSection.appendChild(tags3)
+
+      //botones
+      const accionesBoton = document.createElement('div')
+      accionesBoton.className = 'botonesContenedor'
       const editBoton = document.createElement('button')
       const deleteBoton = document.createElement('button')
+
+      //emojis
+      const emojisContenedor = document.createElement('div')
+      emojisContenedor.className = 'emojisContenedor'
+      const emo1 = document.createElement('p')
+      emo1.className = 'emoticon1'
+      emo1.textContent = '💖'
+      const emo2 = document.createElement('p')
+      emo2.className = 'emoticon2'
+      emo2.textContent = '🦄'
+      const emo3 = document.createElement('p')
+      emo3.className = 'emoticon3'
+      emo3.textContent = '🤩'
+      const emo4 = document.createElement('p')
+      emo4.className = 'emoticon4'
+      emo4.textContent = '🙌'
+      const emo5 = document.createElement('p')
+      emo5.className = 'emoticon5'
+      emo5.textContent = '🔥'
+      const emo6 = document.createElement('p')
+      emo6.className = 'emoticon6'
+      emo6.textContent = '💬 97'
+
+      emojisContenedor.appendChild(emo1)
+      emojisContenedor.appendChild(emo2)
+      emojisContenedor.appendChild(emo3)
+      emojisContenedor.appendChild(emo4)
+      emojisContenedor.appendChild(emo5)
+      emojisContenedor.appendChild(emo6)
+
+      const tiempoContenedor = document.createElement('div')
+      tiempoContenedor.className = 'tiempoContenedor'
+      const tiempo = document.createElement('p')
+      tiempo.type = ''
+      tiempo.textContent = ' 4 min read'
+      tiempo.className = 'tiempo'
+
+      tiempoContenedor.appendChild(tiempo)
+      //botones
 
       celdaTitulo.textContent = item.titulo
       celdaArticulo.textContent = item.articulo
       editBoton.textContent = 'Edit'
       editBoton.className = 'botoncito'
-
       deleteBoton.textContent = 'Delete'
       deleteBoton.className = 'botoncito'
+
+      //eventos//
 
       editBoton.addEventListener('click', function () {
         editInfo(index)
@@ -115,9 +182,23 @@ document.addEventListener('DOMContentLoaded', () => {
       accionesBoton.appendChild(editBoton)
       accionesBoton.appendChild(deleteBoton)
 
+      //contenedor
+      const contenedorReaccionesMas = document.createElement('div')
+      contenedorReaccionesMas.className = 'contenedorReaccionesMas'
+      contenedorReaccionesMas.appendChild(emojisContenedor)
+      contenedorReaccionesMas.appendChild(tiempoContenedor)
+      contenedorReaccionesMas.appendChild(tagsSection)
+
+      //meter todos al contenedor
+      fila.appendChild(divImagen)
+      fila.appendChild(divNombre)
       fila.appendChild(celdaTitulo)
       fila.appendChild(celdaArticulo)
       fila.appendChild(accionesBoton)
+      //fila.appendChild (tagsSection)
+      // fila.appendChild (emojisContenedor)
+      fila.appendChild(contenedorReaccionesMas)
+
       bodyTabla.appendChild(fila)
     })
   }
@@ -138,4 +219,24 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   generarTabla()
+
+  const d = document
+  function searchFilters(input, selector) {
+    d.addEventListener('keyup', (e) => {
+      if (e.target.matches(input)) {
+        // console.log(e.key)
+        // console.log(e.target.value)
+
+        d.querySelectorAll(selector).forEach((el) => {
+          el.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ? el.classList.remove('filter')
+            : el.classList.add('filter')
+          /* if (el.classList.contains('filter')) {el.style.display = 'none'}
+          else  el.style.display = 'grid' */
+        })
+      }
+    })
+  }
+
+  searchFilters('.inputNav', '.contenedorPost')
 })
