0fb66771c3a0a491630e3fc1ebf2f6967d01c79f
Raul Elizalde <raulei2323@gmail.com>
2024-02-11 04:27:09 -0600

    contador de reacciones sin validaciones, sin problemas aparentes git add index.html  css/reactionstyles.css scriptreactions.js git add index.html  css/reactionstyles.css scriptreactions.js en desarrollo

---
+++ scriptreactions.js
@@ -0,0 +1,75 @@
+const likesUp = document.querySelector('#likesUp');
+// const dislikeCount = document.querySelector('#dislikeCount');
+
+function contadorLikes(){
+    likesUp.value = parseInt(likesUp.value) + 1;
+}
+
+// function count(){
+//   dislikeCount.value = parseInt(dislikeCount.value) + 1;
+// }
+
+
+
+//!Ignorar despues borrar si no se va a usar
+// const clickHeart = document.getElementById('heartIcon')
+
+// //funcion que agrega un span al lado de un icono 
+
+// const heartIcon = document.getElementById('heartIcon')
+ 
+ 
+// function newReactionHeart() {
+ 
+//     newReactionHeart  = document.createElement('span')
+//     newReactionHeart.classList.add('reactionCounter')
+//     newReactionHeart.innerText = '1'
+     
+//     heartIcon.append(newReactionHeart)
+// }
+// // eventlistener de la funcion  
+// clickHeart.addEventListener('click', newReactionHeart)
+
+// /////////////////////////////////////
+
+// const clickUnicorn = document.getElementById('unicorn')
+
+// //funcion que agrega un span al lado de un icono 
+
+// const unicornIcon = document.getElementById('unicorn')
+ 
+ 
+// function newReactionUnicorn() {
+ 
+//     newReactionUnicorn = document.createElement('span')
+//     newReactionUnicorn.classList.add('reactionCounter')
+//     newReactionUnicorn.innerText = '1'
+     
+//     unicorn.append(newReactionHeart)
+// }
+// eventlistener de la funcion  
+// clickUnicorn.addEventListener('click', newReactionUnicorn)
+
+
+// const eIcon = document.getElementsByClassName('eIcon')
+
+// function newReaction(e) {
+ 
+//     newReaction = document.createElement('span')
+//     newReaction.classList.add('reactionCounter')
+//     newReaction.innerText = '1'
+//     e.append(newReaction)
+// }
+
+// for ( const icon of eIcon) {
+
+//     eIcon.addEventListener('click', newReaction)
+// }
+
+ 
+
+
+
+
+
+
